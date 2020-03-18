04122308c30fbe6f9f7b95aec0eefcb1f84ed483
vinayakushakola <vinayak.ushakola@gmail.com>
2020-03-18 18:03:43 +0530

    Welcome Message

---
+++ ObjectOrientedProgramming/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ObjectOrientedProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                try
+                {
+                    Console.WriteLine("\n........Welcome To Object Oriented Programs........\n");
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+        }
+    }
+}
