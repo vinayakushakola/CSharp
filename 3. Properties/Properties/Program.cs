cf7cd34b11f9dc7c4c9243bdb8c7b13e2ea4c91d
vinayakushakola <vinayak.ushakola@gmail.com>
2020-03-21 11:34:19 +0530

    C# Properties

---
+++ 3. Properties/Properties/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            School s = new School();
+
+            Console.Write("Enter your id: ");
+            int id = Convert.ToInt32(Console.ReadLine());
+            s.SetID(id);
+
+            Console.Write("Enter your Name: ");
+            s.Name = Console.ReadLine();
+
+            string name2 = s.Name;
+            int id2 = s.GetID();
+            Console.WriteLine("Your id is: ",id2);
+            Console.WriteLine("Your name is: ",name2);
+            Console.WriteLine("Passing marks: {0}", s.PassMark);
+        }
+    }
+}
---
+++ 3. Properties/Properties/School.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Properties
+{
+    public class School
+    {
+        private int _ID;
+        private string _Name;
+        private int _passMark = 35;
+
+        public void SetID(int id)
+
+        {
+            if (id <= 0)
+            {
+                throw new Exception("ID cannot be negative");
+            }
+            this._ID = id;
+        }
+
+        public int GetID()
+        {
+            return this._ID;
+        }
+
+        public string Name
+        {
+            set
+            {
+                if (string.IsNullOrEmpty(Name))
+                {
+                    throw new Exception("Errr");
+                }
+                this._Name = Name;
+            }
+            get
+            {
+                return (string.IsNullOrEmpty(this._Name)) ? "No Name" : this._Name;
+            }
+        }
+
+        public int PassMark
+        {
+            get
+            {
+                return this._passMark;
+            }
+        }
+
+        public string Email { get; set; }
+    }
+}
