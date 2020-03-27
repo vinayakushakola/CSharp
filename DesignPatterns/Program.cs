d88b54f548751e3c8039466f25d7f59a04a6c599
vinayakushakola <vinayak.ushakola@gmail.com>
2020-03-27 14:27:00 +0530

    ADDED: Singleton Link

---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,46 @@
+﻿using DesignPatterns.CreationalDesignPatterns;
+using System;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                bool flag = true;
+                int choice;
+                while (flag)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("\n----------------------Design Pattern Programs----------------------\n");
+                    Console.WriteLine("\t1. Creational Design Patterns");
+                    Console.WriteLine("\t2. Structural Design Patterns");
+                    Console.WriteLine("\t3. Behavioural Design Patterns");
+                    Console.WriteLine("\t4. Exit");
+                    Console.Write("Enter your choice: ");
+                    choice = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine();
+
+                    switch (choice)
+                    {
+                        case 1:
+                            CreationalDesignPatternProgram.CDPatternRun();
+                            break;
+                        case 4:
+                            flag = false;
+                            break;
+                        default:
+                            Console.WriteLine("Invalid choice!");
+                            break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Message: {0}", ex.Message);
+            }
+        }
+    }
+}
