a1c0186a52f8ac5a48f46d29d0934c816b861adf
vinayakushakola <vinayak.ushakola@gmail.com>
2020-03-28 18:44:20 +0530

    ADDED: Prototype Pattern

---
+++ DesignPatterns/CreationalDesignPatterns/PrototypeDesignPattern/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.CreationalDesignPatterns.PrototypeDesignPattern
+{
+    class Employee
+    {
+        private List<string> employeeList;
+
+        public Employee()
+        {
+            employeeList = new List<string>();
+        }
+
+        public Employee(List<string> employee)
+        {
+            employeeList = employee;
+        }
+
+        public void LoadEmployee()
+        {
+            for (int i=1; i<5; i++)
+            {
+                employeeList.Add("Emp " + i);
+            }
+        }
+
+        public List<string> GetEmpData()
+        {
+            return employeeList;
+        }
+
+        public Employee ShallowCopy()
+        {
+            return (Employee)this.MemberwiseClone();
+        }
+    }
+}
---
+++ DesignPatterns/CreationalDesignPatterns/PrototypeDesignPattern/EmployeeProgram.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.CreationalDesignPatterns.PrototypeDesignPattern
+{
+    class EmployeeProgram
+    {
+        public static void EmployeeRun()
+        {
+            try
+            {
+                Console.WriteLine("Output: ");
+                Employee employee = new Employee();
+                employee.LoadEmployee();
+
+                Employee employee1 = employee.ShallowCopy();
+
+                Employee employee2 = employee.ShallowCopy();
+
+
+                List<string> empList = employee.GetEmpData();
+                foreach(String data in empList)
+                {
+                    Console.Write(data + " ");
+                }
+                Console.WriteLine();
+
+                List<string> empList1 = employee.GetEmpData();
+                empList1.Add("Vinayak");
+                foreach (String data in empList1)
+                {
+                    Console.Write(data + " ");
+                }
+                Console.WriteLine();
+
+
+                List<string> empList2 = employee.GetEmpData();
+                empList2.Remove("Vinayak");
+                empList2.Remove("Emp 4");
+
+                foreach (String data in empList2)
+                {
+                    Console.Write(data + " ");
+                }
+                Console.WriteLine();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Message: {0}", ex.Message);
+            }
+        }
+    }
+}
