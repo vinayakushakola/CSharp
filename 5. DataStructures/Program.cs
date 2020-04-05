efb4b60a965bfdcd4ba5183a5784ff96034cbb22
vinayakushakola <vinayak.ushakola@gmail.com>
2020-04-05 13:58:35 +0530

    ADDED: Welcome Message

---
+++ 5. DataStructures/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Purpose: Entry point for the program
+ * @author  Vinayak Ushakola
+ * @version 1.0
+ * @since   05-04-2020
+ */
+
+using System;
+
+namespace DataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("\n---------------Welcome To Data Structures----------------\n");
+                Console.ReadKey();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Message: {0}", ex.Message);
+            }
+        }
+    }
+}
